5893afe26b70ffff70363e529781cef2fc98e2ce
PibiCo <pibico.sl@gmail.com>
2021-11-27 10:17:33 +0100

    modificado:     pibicut_invoice/fixtures/custom_field.json 	modificado:     pibicut_invoice/pibicut_invoice/custom.py

--- pibicut_invoice/pibicut_invoice/custom.py
+++ pibicut_invoice/pibicut_invoice/custom.py
@@ -14,7 +14,7 @@ from PIL import Image
 import base64, os
 from io import BytesIO
 
-def get_qrCode(input_data):
+def get_qrCode(input_data, logo):
   qr = qrcode.QRCode(
         version=6,
         box_size=4,
@@ -22,8 +22,17 @@ def get_qrCode(input_data):
   )
   qr.add_data(input_data)
   qr.make(fit=True)
+  path = frappe.utils.get_bench_path()
+  site_name = frappe.utils.get_url().replace("http://","").replace("https://","")
+  if ":" in site_name:
+    pos = site_name.find(":")
+    site_name = site_name[:pos]
   
-  img = qr.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer(), eye_drawer=SquareModuleDrawer())
+  if logo:
+    embedded = os.path.join(path, "sites", site_name, 'public', logo[1:])
+    img = qr.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer(), eye_drawer=SquareModuleDrawer(), embeded_image_path=embedded)
+  else:
+    img = qr.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer(), eye_drawer=SquareModuleDrawer())
   #qr = qrcode.make(input_str)
   temp = BytesIO()
   img.save(temp, "PNG")
@@ -87,6 +96,6 @@ def generate_tlv_qr(doc, method):
   doc.base64_data = base64_data
   ## Generate and fill TLV QR Code image based on encoded string
   if base64_data:
-    doc.qr_code = get_qrCode(base64_data)
+    doc.qr_code = get_qrCode(base64_data, doc.logo)
   else:
     doc.qr_code = None
